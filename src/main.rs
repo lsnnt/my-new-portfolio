504f041130d941e89db3ad93c8dcf078c5a46890
lsnnt <tnityanand523@gmail.com>
2026-07-22 23:20:43 +0530

    changed .clone() to borrow and added crypto donation addresses

--- src/main.rs
+++ src/main.rs
@@ -18,12 +18,12 @@ type SharedState = Arc<RwLock<AppState>>;
 
 #[derive(Template)] // this will generate the code...
 #[template(path = "index.html")]
-struct IndexTemplate {
-    repos: Vec<Repo>,
-    rating: u16,
-    max_rating: u16,
-    leetcode_problems: u16,
-    blogs: Vec<Blogs>,
+struct IndexTemplate<'a> {
+    repos: &'a Vec<Repo>,
+    rating: &'a u16,
+    max_rating: &'a u16,
+    leetcode_problems: &'a u16,
+    blogs: &'a Vec<Blogs>,
 }
 
 #[get("/")]
@@ -32,18 +32,18 @@ async fn hello(
 ) -> Result<impl Responder, Box<dyn std::error::Error>> {
     let dta = state.read().await;
     let hello = IndexTemplate {
-        repos: dta.repos.clone(),
-        rating : dta.rating.clone(),
-        max_rating: dta.max_rating.clone(),
-        leetcode_problems: dta.leetcode_problems.clone(),
-        blogs: dta.blogs.clone(),
+        repos: &dta.repos,
+        rating : &dta.rating,
+        max_rating: &dta.max_rating,
+        leetcode_problems: &dta.leetcode_problems,
+        blogs: &dta.blogs,
     };
     let body = hello.render()?;
     Ok(HttpResponse::Ok().content_type("text/html").body(body))
 }
 
 async fn update_loop(state: SharedState) -> Result<(), Box<dyn std::error::Error>> {
-    let mut interval = tokio::time::interval(std::time::Duration::from_secs(20 * 60));
+    let mut interval = tokio::time::interval(std::time::Duration::from_secs(10));
 
     loop {
         interval.tick().await;
